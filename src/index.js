266777fdbbd4c7bdc674f656a4bfdd36809cf420
ANDBEKETOV <backwooodz@gmail.com>
2022-09-11 11:23:25 +0200

    index.js angepasst;

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 require("dotenv").config();
-const Discord = require("discord.js");
+const {Client, ActivityType } = require("discord.js");
 
-const client = new Discord.Client({intents: []});
+const client = new Client({intents: []});
 
 client.on("ready", () => {
     console.log(`Ready | Logged in as ${client.user.tag} | I'm on ${client.guilds.cache.size} guild(s) |`);
     client.user.setPresence({
-        activities: [{name: 'test123'}],
-        status: 'online',
+        activities: [{ name: `NETFLIX`, type: ActivityType.Watching }],
+        status: 'online'
     });
-    client.user.setActivity('am Code rum', {type: Discord.Playing});
 });
 
 
