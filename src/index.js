653169ee6cd19c8a6b609a0f416d28c37df73717
ANDBEKETOV <backwooodz@gmail.com>
2022-09-21 21:39:19 +0200

    - discord.js Version erhöht - /ping Befehl hinzugefügt

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-require("dotenv").config();
-const {Client, ActivityType } = require("discord.js");
+const {token, clientId} = require('../config.json');
+const {Client, ActivityType, GatewayIntentBits, REST, Routes} = require('discord.js');
 
-const client = new Client({intents: []});
+const commands = [
+    {
+        name: 'ping',
+        description: 'Let\'s find out what will happen :)!',
+    },
+];
 
-client.on("ready", () => {
-    console.log(`Ready | Logged in as ${client.user.tag} | I'm on ${client.guilds.cache.size} guild(s) |`);
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(Routes.applicationCommands(clientId), { body: commands });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+client.once('ready', () => {
+    console.log(`Ready | Logged in as ${client.user.tag} | I'm on ${client.guilds.cache.size} guild(s)`);
     client.user.setPresence({
-        activities: [{ name: `NETFLIX`, type: ActivityType.Watching }],
+        activities: [{name: `Netflix`, type: ActivityType.Watching}],
         status: 'online'
     });
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Ciao Degga!');
+    }
+});
+
+client.login(token);
